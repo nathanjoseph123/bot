a49bb92f910d352fac0d1fe489371d2f859d02c4
Nathan joseph <152595802+nathanjoseph123@users.noreply.github.com>
2025-12-12 14:26:16 +0100

    New life

--- bolu/luncher.py
+++ bolu/luncher.py
@@ -54,5 +54,8 @@ def get_number():
     bot_number = custom_bot.counter
     return jsonify({"number": bot_number})
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
