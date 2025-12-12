4b8166a77a6ea422938bb98991536bc36129029b
Nathan joseph <152595802+nathanjoseph123@users.noreply.github.com>
2025-12-12 09:40:35 +0100

    new one

---
+++ bolu/luncher.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify, render_template
+import threading, time
+from bot import custom_bot
+
+app = Flask(__name__)
+
+bot_running = False
+bot_starter = None
+bot_number = custom_bot.counter
+bot_timer = 2
+
+@app.route("/")
+def form():
+    return render_template("index.html")
+
+@app.route("/start", methods=["POST"])
+def start():
+    global bot_running, bot_thread, bot_number, bot_timer
+    if bot_running:
+        return jsonify({"status": "Bot already running"})
+
+    data = request.json
+    url = data.get("url")
+    auth = data.get("auth")
+    api_key = data.get("api")
+    special_id = data.get("special")
+    idv = data.get("idv")
+    user_prompt = data.get("user_prompt")
+    bot_timer = max(1, min(60, data.get("timer", 2)))
+
+    bot_running = True
+    try:
+        bot_starter = threading.Thread(
+            target=custom_bot,
+            args=(str(url), str(api_key), str(auth), str(user_prompt), str(idv), bot_timer, str(special_id))
+        )
+        bot_starter.start()
+    except Exception as e:
+        pass
+
+    bot_number = custom_bot.counter
+    return jsonify({"status": f"Bot started with timer {bot_timer}s!\n fuck you bolu"})
+
+@app.route("/stop", methods=["POST"])
+def stop():
+    global bot_running
+    if not bot_running:
+        return jsonify({"status": "Bot is not running"})
+    bot_running = False
+    return jsonify({"status": "Bot stopped"})
+
+@app.route("/number")
+def get_number():
+    bot_number = custom_bot.counter
+    return jsonify({"number": bot_number})
+
+if __name__ == "__main__":
+    app.run(debug=True)
