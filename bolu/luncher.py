0f3aa702fb4fdad7ddfc5c924e6f788188348880
Nathan joseph <152595802+nathanjoseph123@users.noreply.github.com>
2025-12-12 14:43:56 +0100

    New update

--- bolu/luncher.py
+++ bolu/luncher.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-import threading, time
+import threading, time,os
 from bot import custom_bot
 
 app = Flask(__name__)
@@ -58,4 +58,5 @@ def get_number():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
