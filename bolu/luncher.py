8f6e533c04d05677fe4e6efa80c0a6fae9f3f3f8
Nathan joseph <152595802+nathanjoseph123@users.noreply.github.com>
2025-12-14 17:34:41 +0100

    Add parameters to bot.run in start function
    Updated the start function to pass additional arguments to the bot's run method.

--- bolu/luncher.py
+++ bolu/luncher.py
@@ -33,7 +33,7 @@ def start():
 
     bot_running = True
     try:
-        x = Thread(target=bot.run, daemon=True)
+        x = Thread(target=bot.run, args=(str(url), str(api_key), str(auth), str(user_prompt), str(idv), int(bot_timer), str(special_id)),daemon=True)
         x.start()
     except Exception as e:
         pass
@@ -58,6 +58,7 @@ def get_number():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
